b06940ef730bf87ccf84a8da19207142b66bde35
Biswas710 <biswasthap1035@gmail.com>
2026-05-06 13:01:06 +0530

    Day 3: Add expenses view, integrate Thymeleaf rendering, fix missing template issue, and display data from database

--- src/main/java/com/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/expense_tracker/controller/ExpenseController.java
@@ -1,12 +1,19 @@
 package com.expense_tracker.controller;
 
 import com.expense_tracker.model.Expense;
+import com.expense_tracker.repository.ExpenseRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ExpenseController {
+
+    private final ExpenseRepository repo;
+
+    public ExpenseController(ExpenseRepository repo) {
+        this.repo = repo;
+    }
 
     @GetMapping("/add-expense")
     public String showForm(Model model) {
@@ -15,8 +22,14 @@ public class ExpenseController {
     }
 
     @PostMapping("/save-expense")
-    public String saveExpense(@ModelAttribute Expense expense, Model model) {
-        model.addAttribute("message", "Expense Added: " + expense.getTitle());
-        return "result";
+    public String saveExpense(@ModelAttribute Expense expense) {
+        repo.save(expense);
+        return "redirect:/expenses";
+    }
+
+    @GetMapping("/expenses")
+    public String showExpenses(Model model) {
+        model.addAttribute("expenses", repo.findAll());
+        return "expenses";
     }
 }
