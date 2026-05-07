d1bcdccb13b7a8da731d3e29e9b74e8694f24584
Biswas710 <biswasthap1035@gmail.com>
2026-05-07 23:31:16 +0530

    Day 5: Implement complete CRUD operations with edit and delete functionality

--- src/main/java/com/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/expense_tracker/controller/ExpenseController.java
@@ -11,25 +11,70 @@ public class ExpenseController {
 
     private final ExpenseRepository repo;
 
+    // Constructor Injection
     public ExpenseController(ExpenseRepository repo) {
         this.repo = repo;
     }
 
+
     @GetMapping("/add-expense")
     public String showForm(Model model) {
+
+
         model.addAttribute("expense", new Expense());
+
         return "add-expense";
     }
 
+
     @PostMapping("/save-expense")
     public String saveExpense(@ModelAttribute Expense expense) {
+
+
         repo.save(expense);
+
+
         return "redirect:/expenses";
     }
 
+
     @GetMapping("/expenses")
     public String showExpenses(Model model) {
+
+        // Fetch all data from DB
         model.addAttribute("expenses", repo.findAll());
+
         return "expenses";
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String deleteExpense(@PathVariable Long id) {
+
+        // Delete by ID
+        repo.deleteById(id);
+
+        return "redirect:/expenses";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String editExpense(@PathVariable Long id, Model model) {
+        Expense expense = repo.findById(id).orElse(null);
+
+
+        model.addAttribute("expense", expense);
+
+        // Reuse same form
+        return "add-expense";
+    }
+
+
+    @PostMapping("/update-expense")
+    public String updateExpense(@ModelAttribute Expense expense) {
+
+        repo.save(expense);
+
+        return "redirect:/expenses";
     }
 }
